6d000378f02f2d1673b6c95d5a1750c7506ebfa6
Juan Alorro <jralorro93@gmail.com>
2019-02-18 18:42:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ end
 
 def get_japanese_emoticon(path, emoticon)
   lib_hash = load_library(path)
-  dictionary = lib_hash
+  dictionary = lib_hash['get_emoticon']
 end
 
 def get_english_meaning(path, emoticon)
